398bf2069ee1f8ea39028f8914f2269c3264f53d
Sarcoma <sarcoma@live.co.uk>
2020-08-16 20:02:05 +0100

    add __main__.py

---
+++ baduk/__main__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+from baduk.main import Main
+
+
+def main():
+    try:
+        Main()
+    except KeyboardInterrupt:
+        sys.exit()
+
+
+if __name__ == "__main__":
+    main()
