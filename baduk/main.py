5624041f83fae65645ec9b8e103d1058d1ee1b8e
Sarcoma <sarcoma@live.co.uk>
2020-08-16 20:25:36 +0100

    add shebang

--- baduk/main.py
+++ baduk/main.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from baduk.dialog.menu_action_dialog import MenuActionDialog
 from baduk.ui.user_menu import UserMenu
 
 
 class Main:
-
     def __init__(self):
         while True:
             if UserMenu(MenuActionDialog.main_menu())() == 'q':
